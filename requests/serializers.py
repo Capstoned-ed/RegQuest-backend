2470ff27a9bcfb0fabcd2d0a8608985be908edd7
Gabecx <jessicagabica54@gmail.com>
2026-05-02 20:07:50 +0800

    AppDev: DRF backend for system implemented

--- requests/serializers.py
+++ requests/serializers.py
@@ -5,5 +5,4 @@ class RequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = '__all__'
-
-        
+        read_only_fields = ['user', 'status', 'created_at', 'updated_at', 'processed_by']
